88bada5d877216e569b7f414eee7aa9cb6beeafb
Meerthika <meerthikasrrajendrans@gmail.com>
2025-04-07 17:47:43 +0530

    Add Patient handlers

--- patients_refined/handlers/patient_handler.go
+++ patients_refined/handlers/patient_handler.go
@@ -1,2 +1,49 @@
 package handlers
 
+import(
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"patients_refined/models"
+	"patients_refined/storage"
+)
+
+func GetAllPatients(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(storage.Patients)
+}
+
+func GetPatientByID(w http.ResponseWriter, r *http.Request){
+	idStr:= r.URL.Path[len("/patient/"):]
+	id,err:= strconv.Atoi(idStr)
+
+	if err!=nil{
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	for _, p := range storage.Patients{
+		if p.ID == id {
+			json.NewEncoder(w).Encode(p)
+			return
+		}
+	}
+
+	http.Error(w, "Patient not found", http.StatusNotFound)
+}
+
+
+func AddPatient(w http.ResponseWriter, r * http.Request){
+	var p models.Patient
+	err:=json.NewDecoder(r.Body).Decode(&p)
+
+	if err != nil{
+		http.Error(w, "Invalid data", http.StatusBadRequest)
+	}
+
+	p.ID = storage.NextID
+	storage.NextID++
+	storage.Patients = append(storage.Patients, p)
+	json.NewEncoder(w).Encode(p)
+}
